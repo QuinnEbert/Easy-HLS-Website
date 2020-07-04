70ade80af499ffa55f6b3599f3a1e61eab1ab2a9
Quinn Ebert <quinnebert@gmail.com>
2020-07-04 07:17:27 -0400

    Update Ubuntu-20.04-Install.sh
    Remove broken dpkg/apt crap from the 20.04 script, sorry y'all

--- easy-installs/Ubuntu-20.04-Install.sh
+++ easy-installs/Ubuntu-20.04-Install.sh
@@ -3,11 +3,8 @@ if (( $EUID != 0 )); then
   echo 'This script only works when run as root!'
   exit
 fi
-dpkg --clear-selections
-dpkg --set-selections < Ubuntu-20.04-dpkg_selections.txt
+
 chmod +x Ubuntu-20.04-InstallHelper.php
-apt-get -y autoremove
-apt-get -y dselect-upgrade
 mv Ubuntu-20.04-fs_overlay.tar.bz2 /
 export MOVEBACKTO=`pwd`
 cd /
