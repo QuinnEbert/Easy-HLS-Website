dd7ff58b748915095f684c50dab39b8a3597f6c2
Quinn Ebert <quinnebert@gmail.com>
2020-07-04 08:29:35 -0400

    Update Ubuntu-20.04-Install.sh
    Crap, I restarted nginx too early, but now, this should work flawlessly

--- easy-installs/Ubuntu-20.04-Install.sh
+++ easy-installs/Ubuntu-20.04-Install.sh
@@ -9,9 +9,9 @@ mv Ubuntu-20.04-fs_overlay.tar.bz2 /
 export MOVEBACKTO=`pwd`
 cd /
 tar -xjf Ubuntu-20.04-fs_overlay.tar.bz2
-systemctl enable nginx
-systemctl restart nginx
 mv /Ubuntu-20.04-fs_overlay.tar.bz2 "$MOVEBACKTO"
 cd "$MOVEBACKTO"
 ./Ubuntu-20.04-InstallHelper.php
 echo 'Install completed, check the README for next steps'
+systemctl enable nginx
+systemctl restart nginx
